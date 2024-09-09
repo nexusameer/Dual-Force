7bad9cd4b5942bbb5fe38c2ac47b6c8237d5d86b
nexusameer <ameerk10fw@gmail.com>
2024-09-09 02:15:38 -0700

    deploy

--- TinTosh_Web/settings.py
+++ TinTosh_Web/settings.py
@@ -145,3 +145,5 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+
+django_heroku.settings(locals())
